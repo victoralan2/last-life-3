5514591451966ade632774b9f9a7bb35683818aa
Mario_55 <mario.gandia.caballero@gmail.com>
2023-02-25 20:55:09 +0100

    TotemDeReclamo is done.

--- src/main/java/net/olimpium/last_life_iii/items/Totems/TotemDeReclamo.java
+++ src/main/java/net/olimpium/last_life_iii/items/Totems/TotemDeReclamo.java
@@ -1,23 +1,31 @@
 package net.olimpium.last_life_iii.items.Totems;
 
+import net.minecraft.server.v1_16_R3.EntityPlayer;
+import net.minecraft.server.v1_16_R3.NBTTagCompound;
 import net.olimpium.last_life_iii.Last_life_III;
 import net.olimpium.last_life_iii.utils.InventoryUtils;
 import net.olimpium.last_life_iii.utils.VerificationSystem;
 import org.bukkit.*;
 import org.bukkit.block.*;
 import org.bukkit.block.data.Directional;
+import org.bukkit.craftbukkit.v1_16_R3.entity.CraftPlayer;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityResurrectEvent;
+import org.bukkit.event.player.PlayerBedEnterEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.util.Vector;
 
+import java.util.Arrays;
 import java.util.Objects;
 import java.util.Random;
 
+import static org.bukkit.persistence.PersistentDataType.TAG_CONTAINER;
 
 
 public class TotemDeReclamo implements Listener {
@@ -30,9 +38,9 @@ public class TotemDeReclamo implements Listener {
                     @Override
                     public void run() {
                         player.stopSound(Sound.ITEM_TOTEM_USE);
-                        player.getWorld().playSound(player.getLocation(), Sound.ENTITY_ENDERMAN_TELEPORT, 1f,0.85f);
+                        player.getWorld().playSound(player.getLocation(), Sound.ENTITY_ENDERMAN_TELEPORT, 1f, 0.85f);
                         player.teleport(player.getBedSpawnLocation());
-                        player.getWorld().playSound(player.getLocation(), Sound.BLOCK_RESPAWN_ANCHOR_SET_SPAWN, 1f,1.5f);
+                        player.getWorld().playSound(player.getLocation(), Sound.BLOCK_RESPAWN_ANCHOR_SET_SPAWN, 1f, 1.5f);
 
                     }
                 }.runTaskLater(Last_life_III.getPlugin(), 2);
@@ -41,21 +49,22 @@ public class TotemDeReclamo implements Listener {
                     @Override
                     public void run() {
                         player.stopSound(Sound.ITEM_TOTEM_USE);
-                        player.getWorld().playSound(player.getLocation(), Sound.ENTITY_ENDERMAN_TELEPORT, 1f,0.85f);
+                        player.getWorld().playSound(player.getLocation(), Sound.ENTITY_ENDERMAN_TELEPORT, 1f, 0.85f);
                         player.teleport(VerificationSystem.normalWorld.getSpawnLocation());
-                        player.getWorld().playSound(player.getLocation(), Sound.BLOCK_RESPAWN_ANCHOR_SET_SPAWN, 1f,1.5f);
+                        player.getWorld().playSound(player.getLocation(), Sound.BLOCK_RESPAWN_ANCHOR_SET_SPAWN, 1f, 1.5f);
                     }
                 }.runTaskLater(Last_life_III.getPlugin(), 2);
             }
         } else {
-            if(player.getBedLocation() != null){
-
-                Block block = player.getWorld().getBlockAt(player.getBedSpawnLocation());
+            if (player.getBedSpawnLocation() != null) {
+                int[] integers = player.getPersistentDataContainer().get(new NamespacedKey(Last_life_III.getPlugin(), "bedLocation"), PersistentDataType.INTEGER_ARRAY);
+                Block block = new Location(VerificationSystem.normalWorld,integers[0],integers[1],integers[2]).getBlock();
                 Block block1 = null;
                 Directional bed = (Directional) block.getBlockData();
-                block1 = block.getLocation().add(bed.getFacing().getDirection().getX(), 0, bed.getFacing().getDirection().getZ()).getBlock();
+                block1 = block.getLocation().add(-bed.getFacing().getDirection().getX(), 0, -bed.getFacing().getDirection().getZ()).getBlock();
 
                 block.setType(Material.BARREL);
+                block1.setType(Material.BARREL);
 
 
                 BlockState blockState = block.getState();
@@ -64,20 +73,20 @@ public class TotemDeReclamo implements Listener {
                 BlockState blockState1 = Objects.requireNonNull(block1).getState();
                 Barrel barrel1 = (Barrel) blockState1;
 
-                for (ItemStack itemStack : player.getInventory().getContents()){
+                for (ItemStack itemStack : player.getInventory().getContents()) {
                     if (itemStack == null) continue;
-                    if(!InventoryUtils.isFull(barrel.getInventory())) {
+                    if (!InventoryUtils.isFull(barrel.getInventory())) {
                         barrel.getInventory().addItem(itemStack);
-                    }else{
+                    } else {
                         barrel1.getInventory().addItem(itemStack);
                     }
                 }
                 player.getInventory().clear();
 
 
-            }else{
+            } else {
                 Block block = VerificationSystem.normalWorld.getSpawnLocation().getBlock();
-                Block block1 = block.getLocation().add(0,1,0).getBlock();
+                Block block1 = block.getLocation().add(0, 1, 0).getBlock();
                 block.setType(Material.BARREL);
                 block1.setType(Material.BARREL);
 
@@ -88,20 +97,20 @@ public class TotemDeReclamo implements Listener {
                 BlockState blockState1 = Objects.requireNonNull(block1).getState();
                 Barrel barrel1 = (Barrel) blockState1;
 
-                if(!barrel.getInventory().isEmpty()||!barrel1.getInventory().isEmpty()){
-                    if(!barrel.getInventory().isEmpty()) {
+                if (!barrel.getInventory().isEmpty() || !barrel1.getInventory().isEmpty()) {
+                    if (!barrel.getInventory().isEmpty()) {
                         for (ItemStack itemStack : barrel.getInventory().getContents()) {
                             if (itemStack == null) continue;
                             Item item = barrel.getWorld().dropItem(barrel.getLocation().add(0.5, 2, 0.5), itemStack);
-                            item.setVelocity(new Vector(0,0,0));
+                            item.setVelocity(new Vector(0, 0, 0));
                             barrel.getInventory().clear();
                         }
                     }
-                    if(!barrel1.getInventory().isEmpty()){
-                        for (ItemStack itemStack : barrel1.getInventory().getContents()){
-                            if(itemStack == null) continue;
-                            Item item = barrel1.getWorld().dropItem(barrel1.getLocation().add(0.5,1,0.5),itemStack);
-                            item.setVelocity(new Vector(0,0,0));
+                    if (!barrel1.getInventory().isEmpty()) {
+                        for (ItemStack itemStack : barrel1.getInventory().getContents()) {
+                            if (itemStack == null) continue;
+                            Item item = barrel1.getWorld().dropItem(barrel1.getLocation().add(0.5, 1, 0.5), itemStack);
+                            item.setVelocity(new Vector(0, 0, 0));
                             barrel1.getInventory().clear();
                         }
                     }
@@ -120,9 +129,10 @@ public class TotemDeReclamo implements Listener {
             }
         }
     }
+
     @EventHandler
-    public void onTotemActivate(EntityResurrectEvent e){
-        if(!(e.getEntity() instanceof Player)) return;
+    public void onTotemActivate(EntityResurrectEvent e) {
+        if (!(e.getEntity() instanceof Player)) return;
         Player player = ((Player) e.getEntity());
         if (player.getInventory().getItemInMainHand().getItemMeta() != null) {
             if (player.getInventory().getItemInMainHand().getItemMeta().getDisplayName().equals(ChatColor.AQUA.toString() + ChatColor.BOLD + "Tótem de Reclamo")) {
@@ -145,5 +155,13 @@ public class TotemDeReclamo implements Listener {
                 }
             }
         }
+    }
+
+    @EventHandler
+    public void sleepTagAdder(PlayerBedEnterEvent e) {
+        Player player = e.getPlayer();  // Get a Bukkit entity.
+
+        player.getPersistentDataContainer().set(new NamespacedKey(Last_life_III.getPlugin(), "bedLocation"), PersistentDataType.INTEGER_ARRAY, new int[]{e.getBed().getX(),e.getBed().getY(),e.getBed().getZ()});
+        Bukkit.broadcastMessage(Arrays.toString(player.getPersistentDataContainer().get(new NamespacedKey(Last_life_III.getPlugin(), "bedLocation"), PersistentDataType.INTEGER_ARRAY)));
     }
 }
