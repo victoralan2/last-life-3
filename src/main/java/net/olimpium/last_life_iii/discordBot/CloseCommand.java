48cc934186418023a6ebb5150323e61ed7764c96
Mario_55 <mario.gandia.caballero@gmail.com>
2023-03-07 22:10:41 +0100

    Added CloseCommand on Discord Bot

--- src/main/java/net/olimpium/last_life_iii/discordBot/CloseCommand.java
+++ src/main/java/net/olimpium/last_life_iii/discordBot/CloseCommand.java
@@ -2,31 +2,54 @@ package net.olimpium.last_life_iii.discordBot;
 
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.SubscribeEvent;
+import net.olimpium.last_life_iii.Last_life_III;
 import net.olimpium.last_life_iii.utils.TimeSystem;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.Objects;
 
 public class CloseCommand {
     @SubscribeEvent
     public void onCloseCommand(SlashCommandInteractionEvent event){
-
-        // TODO: hazer esto
         if(!event.getName().equals("maintenance")) return;
+
         if(!TimeSystem.getIsInMaintenance()) {
             TimeSystem.setIsInMaintenance(true);
-            for (Player player : Bukkit.getOnlinePlayers()) {
-                if (player.isOp()) continue;
-                    player.kickPlayer(ChatColor.RED + "EL SERVIDOR ESTA EN MANTENIMIENTO\n" + ChatColor.YELLOW + " Consulte #anuncios para saber más información.");
+
+            if(event.getOption("tiempo")==null){
+
+                Bot.serverMaintenance(true, null);
+                event.reply("Last Life has been closed to the public").setEphemeral(true).queue();
+                new BukkitRunnable() {
+                    @Override
+                    public void run() {
+                        for (Player player : Bukkit.getOnlinePlayers()) {
+                            if (player.isOp()) continue;
+                            player.kickPlayer(ChatColor.RED + "EL SERVIDOR ESTA EN MANTENIMIENTO\n" + ChatColor.YELLOW + " Consulte #anuncios para saber más información.");
+                        }
+                    }
+                }.runTask(Last_life_III.getPlugin());
+
+            }else{
+                Bot.serverMaintenance(true, Objects.requireNonNull(event.getOption("tiempo")).getAsInt());
+                event.reply("Last Life has been closed to the public, estimated time: "+ Objects.requireNonNull(event.getOption("tiempo")).getAsInt()+" minutes").setEphemeral(true).queue();
+                new BukkitRunnable() {
+                    @Override
+                    public void run() {
+                        for (Player player : Bukkit.getOnlinePlayers()) {
+                            if (player.isOp()) continue;
+                            player.kickPlayer(ChatColor.RED + "EL SERVIDOR ESTA EN MANTENIMIENTO" + "\n" + ChatColor.GOLD + "TIEMPO ESPERADO: " + ChatColor.BOLD + Objects.requireNonNull(event.getOption("tiempo")).getAsInt() + " MINUTOS" + ChatColor.YELLOW + ChatColor.ITALIC + "\nConsulte #anuncios para poder saber más información.");
+                        }
+                    }
+                }.runTask(Last_life_III.getPlugin());
+
             }
-        } else{
+        }else{
             TimeSystem.setIsInMaintenance(false);
             Bot.serverMaintenance(false, null);
-        }
-        if(event.getOption("tiempo") != null){
-            Bot.serverMaintenance(true, event.getOption("tiempo").getAsInt());
-        }else{
-            Bot.serverMaintenance(true, null);
         }
     }
 }
