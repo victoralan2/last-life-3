e04e647d4cd87c9e5173f88dc36e96a4cb18166a
Mario_55 <mario.gandia.caballero@gmail.com>
2023-03-15 22:02:18 +0100

    Added new mob `Espectro de la Codicia`

--- src/main/java/net/olimpium/last_life_iii/mobs/EspectroDeCodicia.java
+++ src/main/java/net/olimpium/last_life_iii/mobs/EspectroDeCodicia.java
@@ -1,18 +1,22 @@
 package net.olimpium.last_life_iii.mobs;
 
+import net.minecraft.server.v1_16_R3.EntityTypes;
+import net.olimpium.last_life_iii.Last_life_III;
 import net.olimpium.last_life_iii.utils.EXPUtils;
 import net.olimpium.last_life_iii.utils.EntityNBTManager;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Particle;
+import org.bukkit.Sound;
 import org.bukkit.attribute.Attribute;
-import org.bukkit.entity.LivingEntity;
-import org.bukkit.entity.Phantom;
-import org.bukkit.entity.Player;
+import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.entity.EntitySpawnEvent;
 import org.bukkit.persistence.PersistentDataType;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public class EspectroDeCodicia implements Listener {
 	@EventHandler
@@ -23,6 +27,16 @@ public class EspectroDeCodicia implements Listener {
 				phantom.setCustomName(ChatColor.GOLD+"Espectro de la Codicia");
 				phantom.getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(40);
 				phantom.setHealth(40);
+
+				new BukkitRunnable() {
+					@Override
+					public void run() {
+						if (!e.getEntity().isDead()) {
+							e.getEntity().getWorld().spawnParticle(Particle.TOTEM, e.getEntity().getLocation(), 1, 0.5, 0.1, 0.5, 0);
+							e.getEntity().getWorld().spawnParticle(Particle.COMPOSTER, e.getEntity().getLocation(), 5, 0.5, 0.1, 0.5, 0);
+						}
+					}
+				}.runTaskTimer(Last_life_III.getPlugin(), 0, 1);
 			}
 		}
 	}
@@ -34,18 +48,22 @@ public class EspectroDeCodicia implements Listener {
 			if(player.getLevel()<1) return;
 			float expBefore = EXPUtils.getPlayerExp(player);
 			player.setLevel(player.getLevel()-1);
+			player.getWorld().playSound(player.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP,1,0);
 			float expAfter = EXPUtils.getPlayerExp(player);
 			float levelEXP = expBefore-expAfter;
-			Bukkit.broadcastMessage("EXP BEFORE: " + expBefore);
-			Bukkit.broadcastMessage("EXP AFTER: " + expAfter);
+
 			if(EntityNBTManager.readNBT((LivingEntity) e.getDamager(),PersistentDataType.FLOAT,"EXPStolen",Float.class) != null){
 				float levelsStolen = EntityNBTManager.readNBT((LivingEntity) e.getDamager(), PersistentDataType.FLOAT,"EXPStolen",Float.class);
 				EntityNBTManager.writeNBT((LivingEntity) e.getDamager(), PersistentDataType.FLOAT, levelEXP + levelsStolen,"EXPStolen");
-				Bukkit.broadcastMessage("2.Actual EXP: "+levelsStolen+levelEXP);
 			}else{
 				EntityNBTManager.writeNBT((LivingEntity) e.getDamager(), PersistentDataType.FLOAT,levelEXP,"EXPStolen");
-				Bukkit.broadcastMessage("1.Actual EXP: "+levelEXP);
 			}
 		}
+	}
+	@EventHandler
+	public void onDeath(EntityDeathEvent e){
+		if (e.getEntity().getCustomName() == null) return;
+		if(!(e.getEntity().getCustomName().equals(ChatColor.GOLD+"Espectro de la Codicia"))) return;
+		e.setDroppedExp(Math.round(EntityNBTManager.readNBT(e.getEntity(), PersistentDataType.FLOAT,"EXPStolen",Float.class)));
 	}
 }
