2041a5e8353c0bc999c90b6ac199541faebfe9b4
Mario_55 <mario.gandia.caballero@gmail.com>
2023-03-15 22:20:49 +0100

    Balanced bats, may take more than 1 hit to kill a bat.

--- src/main/java/net/olimpium/last_life_iii/mecanicas/BatDropsMoreEXP.java
+++ src/main/java/net/olimpium/last_life_iii/mecanicas/BatDropsMoreEXP.java
@@ -1,12 +1,18 @@
 package net.olimpium.last_life_iii.mecanicas;
 
-import org.bukkit.Bukkit;
+import net.olimpium.last_life_iii.utils.EXPUtils;
+import org.bukkit.Location;
+import org.bukkit.Sound;
+import org.bukkit.entity.Bat;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
+
+import java.util.Random;
 
 
 public class BatDropsMoreEXP implements Listener {
@@ -15,10 +21,21 @@ public class BatDropsMoreEXP implements Listener {
         if(e.getEntity().getType().equals(EntityType.BAT)){
             if(e.getEntity().getKiller() != null){
                Player PlayerKiller = e.getEntity().getKiller();
-               PlayerKiller.setExp(0);
-               PlayerKiller.giveExp(PlayerKiller.getExpToLevel());
+               e.setDroppedExp(EXPUtils.getExpToLevelUp(PlayerKiller.getLevel()+1));
             }
         }
 
+    }
+    @EventHandler
+    public void onBatHit(EntityDamageByEntityEvent e){
+        if(!(e.getEntity() instanceof Bat)) return;
+        if(e.getDamager() instanceof Player){
+            java.util.Random rnd = new Random();
+            if((rnd.nextFloat()*100)<75){
+                e.getEntity().getWorld().playSound(e.getEntity().getLocation(), Sound.ENTITY_PLAYER_ATTACK_NODAMAGE,1,0);
+                e.setCancelled(true);
+            }
+
+        }
     }
 }
