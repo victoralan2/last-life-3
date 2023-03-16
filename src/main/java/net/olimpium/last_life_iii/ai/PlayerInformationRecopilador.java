8fec0c876054ac768f83e5d254af451efd5b8fd4
TheDracon <vicvarcas2007@gmail.com>
2023-03-16 20:18:45 +0100

    Made AI work

--- src/main/java/net/olimpium/last_life_iii/ai/PlayerInformationRecopilador.java
+++ src/main/java/net/olimpium/last_life_iii/ai/PlayerInformationRecopilador.java
@@ -3,10 +3,7 @@ package net.olimpium.last_life_iii.ai;
 import net.olimpium.last_life_iii.Last_life_III;
 import net.olimpium.last_life_iii.utils.DataManager;
 import net.olimpium.last_life_iii.utils.VerificationSystem;
-import org.bukkit.Bukkit;
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.World;
+import org.bukkit.*;
 import org.bukkit.block.Block;
 import org.bukkit.entity.EnderCrystal;
 import org.bukkit.entity.EntityType;
@@ -16,7 +13,10 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.inventory.InventoryOpenEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerVelocityEvent;
 import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.util.Vector;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.ParseException;
@@ -26,15 +26,23 @@ import java.awt.geom.Point2D;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Random;
 import java.util.UUID;
 
 public class PlayerInformationRecopilador implements Listener {
 	public static HashMap<UUID, JSONArray> playersBlocks = new HashMap<>();
-	public static final double phi = 0.01;
-	public static File dataFile = new File(Last_life_III.getPlugin().getDataFolder() + "/playerdata");
+	// THE HIGHER THIS NUMBER THE HIGHER THE REMOVAL OF A BLOCK WITH LITTLE DECAY
+	public static final double PHI = 0.1;
+	// THE HIGHER THIS NUMBER THE LOWER THE DECAY
+	public static final double DECAY_MULTIPLIER = 10;
+	// THE HIGHER THIS NUMBER THE HIGHER REINFORCEMENT WILL DECAY
+	public static final double REINFORCEMENT_DECAY = 1.5;
+	// THE REINFORCEMENT CAP
+	public static final double REINFORCEMENT_CAP = 2;
+	public static final File dataFile = new File(Last_life_III.getPlugin().getDataFolder() + "/playerdata");
 	public static HashMap<UUID, DataManager> uuidjsonObjectHashMap = new HashMap<>();
 	public static void addPlayer(UUID player){
-		DataManager playerDataManager = new DataManager(dataFile.getAbsolutePath() + "/" + player + "_blocks.json");
+		DataManager playerDataManager = new DataManager(dataFile.getAbsolutePath() + "/" + player + "_dataPoints.json");
 		JSONObject object = new JSONObject();
 		playerDataManager.setData(object);
 		uuidjsonObjectHashMap.put(player, playerDataManager);
@@ -44,7 +52,7 @@ public class PlayerInformationRecopilador implements Listener {
 		System.out.println("UUID: " + player);
 		DataManager playerDataManager = new DataManager(file.getAbsolutePath());
 		playerDataManager.loadDataFromFile();
-		playersBlocks.put(player, (JSONArray) playerDataManager.getData().get("blocks"));
+		playersBlocks.put(player, (JSONArray) playerDataManager.getData().get("dataPoints"));
 		uuidjsonObjectHashMap.put(player, playerDataManager);
 		Last_life_III.metaDataManager.addDataManager(playerDataManager);
 		System.out.println("JSONSTRING: " + playerDataManager.getData().toJSONString());
@@ -62,44 +70,102 @@ public class PlayerInformationRecopilador implements Listener {
 		obj.put("X", e.getBlock().getX());
 		obj.put("Y", e.getBlock().getY());
 		obj.put("Z", e.getBlock().getZ());
-		obj.put("multiplier", getMultiplier(e.getBlock().getType()));
+		obj.put("materialMultiplier", getMultiplier(e.getBlock().getType()));
 		obj.put("epoch-second", System.currentTimeMillis()/1000);
-
+		obj.put("reinforcement", 1d);
 		System.out.println(obj.toJSONString());
 		if (!uuidjsonObjectHashMap.containsKey(e.getPlayer().getUniqueId())){
 			addPlayer(e.getPlayer().getUniqueId());
-			JSONArray blocks = new JSONArray();
+			JSONArray dataPoints = new JSONArray();
 
-			blocks.add(obj);
-			playersBlocks.putIfAbsent(e.getPlayer().getUniqueId(), blocks);
-			uuidjsonObjectHashMap.get(e.getPlayer().getUniqueId()).changeData("blocks", blocks);
+			dataPoints.add(obj);
+			playersBlocks.putIfAbsent(e.getPlayer().getUniqueId(), dataPoints);
+			uuidjsonObjectHashMap.get(e.getPlayer().getUniqueId()).changeData("dataPoints", dataPoints);
 		} else {
-			JSONArray playerBlocks = playersBlocks.get(e.getPlayer().getUniqueId());
-			if (playerBlocks == null){
+			JSONArray playerDataPoints = playersBlocks.get(e.getPlayer().getUniqueId());
+			if (playerDataPoints == null){
 				System.out.println("IS NULL!!");
 				playersBlocks.put(e.getPlayer().getUniqueId(), new JSONArray());
 				return;
 			}
-			playerBlocks.add(obj);
-			playersBlocks.replace(e.getPlayer().getUniqueId(), playerBlocks);
-			uuidjsonObjectHashMap.get(e.getPlayer().getUniqueId()).changeData("blocks", playerBlocks);
+			playerDataPoints.add(obj);
+			playersBlocks.replace(e.getPlayer().getUniqueId(), playerDataPoints);
+			uuidjsonObjectHashMap.get(e.getPlayer().getUniqueId()).changeData("dataPoints", playerDataPoints);
 		}
 	}
+	@EventHandler
 	public void onPlayerOpenChest(PlayerInteractEvent e) {
+		if (e.getClickedBlock() == null) return;
+		if (!e.getClickedBlock().getWorld().getEnvironment().equals(World.Environment.NORMAL)) return;
+		e.getPlayer().sendMessage("A");
+
 		if (e.getClickedBlock().getType().equals(Material.CHEST)
 				|| e.getClickedBlock().getType().equals(Material.BARREL)
 				|| e.getClickedBlock().getType().equals(Material.FURNACE)
-				|| e.getClickedBlock().getType().equals(Material.ENCHANTING_TABLE)) {
-			JSONArray playerBlocks = playersBlocks.get(e.getPlayer().getUniqueId());
-			if (playerBlocks == null) {
-				System.out.println("IS NULL!!");
-				playersBlocks.put(e.getPlayer().getUniqueId(), new JSONArray());
-				return;
+				|| e.getClickedBlock().getType().equals(Material.ENCHANTING_TABLE)
+				|| e.getClickedBlock().getType().toString().endsWith("_BED")) {
+			e.getPlayer().sendMessage("B");
+
+			if (!playersBlocks.containsKey(e.getPlayer().getUniqueId()) || !uuidjsonObjectHashMap.containsKey(e.getPlayer().getUniqueId())) return;
+			e.getPlayer().sendMessage("Clicked");
+
+			JSONArray playerDataPoints = playersBlocks.get(e.getPlayer().getUniqueId());
+			int i = 0;
+			for (Object object : (JSONArray) playerDataPoints.clone()){
+				if (object == null) {
+					throw new RuntimeException("Object is null at i = " + i);
+				}
+				JSONObject jsonObject = (JSONObject) object;
+				if (Integer.parseInt(jsonObject.get("X").toString()) == e.getClickedBlock().getX()
+						&& Integer.parseInt(jsonObject.get("Y").toString()) == e.getClickedBlock().getY()
+						&& Integer.parseInt(jsonObject.get("Z").toString()) == e.getClickedBlock().getZ()){
+					jsonObject.replace("epoch-second", System.currentTimeMillis()/1000);
+					jsonObject.replace("reinforcement", Double.parseDouble(jsonObject.get("reinforcement").toString()) + 0.1d);
+					System.out.println((Long.parseLong(jsonObject.get("epoch-second").toString()) == System.currentTimeMillis()/1000) + " ");
+					playerDataPoints.remove(i);
+					playerDataPoints.add(i, jsonObject);
+				}
+				i++;
 			}
-			//playerBlocks.add(obj);
-			playersBlocks.replace(e.getPlayer().getUniqueId(), playerBlocks);
-			uuidjsonObjectHashMap.get(e.getPlayer().getUniqueId()).changeData("blocks", playerBlocks);
+
+			playersBlocks.replace(e.getPlayer().getUniqueId(), playerDataPoints);
+			uuidjsonObjectHashMap.get(e.getPlayer().getUniqueId()).changeData("dataPoints", playerDataPoints);
 		}
+	}
+	@EventHandler
+	public void onPlayerMove(PlayerMoveEvent e) {
+
+
+		Random random = new Random();
+		if (random.nextInt(10) != 1) return;
+		Vector velocity = new Vector(e.getFrom().getX() - e.getTo().getX(),e.getFrom().getY() - e.getTo().getY(), e.getFrom().getZ() - e.getTo().getZ());
+		if (Math.abs(velocity.getX()) <= 0.15 && Math.abs(velocity.getZ()) <= 0.15) return;
+		if (e.getTo() == null) return;
+		if (!e.getPlayer().getWorld().getEnvironment().equals(World.Environment.NORMAL)) return;
+
+		if (!playersBlocks.containsKey(e.getPlayer().getUniqueId()) || !uuidjsonObjectHashMap.containsKey(e.getPlayer().getUniqueId())) return;
+
+		JSONArray playerDataPoints = playersBlocks.get(e.getPlayer().getUniqueId());
+		int i = 0;
+		for (Object object : (JSONArray) playerDataPoints.clone()){
+			if (object == null) {
+				throw new RuntimeException("Object is null at i = " + i);
+			}
+			JSONObject jsonObject = (JSONObject) object;
+			if (Math.abs(Integer.parseInt(jsonObject.get("X").toString())-e.getFrom().getX()) <= 10
+					&& Math.abs(Integer.parseInt(jsonObject.get("Y").toString())-e.getFrom().getY()) <= 10
+					&& Math.abs(Integer.parseInt(jsonObject.get("Z").toString())-e.getFrom().getZ()) <= 10){
+				jsonObject.replace("epoch-second", (Integer.parseInt(jsonObject.get("epoch-second").toString()) + System.currentTimeMillis()/1000) / 2);
+				jsonObject.replace("reinforcement", Double.parseDouble(jsonObject.get("reinforcement").toString()) + 0.05d);
+
+				playerDataPoints.remove(i);
+				playerDataPoints.add(i, jsonObject);
+			}
+			i++;
+		}
+
+		playersBlocks.replace(e.getPlayer().getUniqueId(), playerDataPoints);
+		uuidjsonObjectHashMap.get(e.getPlayer().getUniqueId()).changeData("dataPoints", playerDataPoints);
 	}
 
 	public static Point2D guessCentroid(Player player){
@@ -107,20 +173,82 @@ public class PlayerInformationRecopilador implements Listener {
 
 		ArrayList<Point> points = new ArrayList<>();
 		ArrayList<Double> multipliers = new ArrayList<>();
-		for (Object object : array){
+		for (Object object : (JSONArray) array.clone()){
+			if (object == null) continue;
 			JSONObject jsonObject = (JSONObject) object;
 			Point point = new Point(Integer.parseInt(jsonObject.get("X").toString()), Integer.parseInt(jsonObject.get("Z").toString()));
+			Double reinforcement = Double.parseDouble(jsonObject.get("reinforcement").toString());
+
 			// Decay of block to make older blocks less important
-			double decay = calculateDecay(Integer.parseInt(jsonObject.get("epoch-second").toString()));
-			if (decay<= phi){
+			double decay = calculateDecay(Integer.parseInt(jsonObject.get("epoch-second").toString()), reinforcement);
+			if (decay<= PHI){
+				Bukkit.broadcastMessage("REMOVED!");
 				array.remove(object);
 				continue;
 			}
-			multipliers.add(Integer.parseInt(jsonObject.get("multiplier").toString()) * decay);
+
+			// materialMultiplier * ( decay / reinforcement )
+
+			Double multiplier = Integer.parseInt(jsonObject.get("materialMultiplier").toString()) * decay;
+			Bukkit.broadcastMessage("reinforcement to " + reinforcement);
+			Bukkit.broadcastMessage("decay to " + decay);
+			Bukkit.broadcastMessage("totalMultiplier to " + multiplier);
+
+			multipliers.add(multiplier);
 			points.add(point);
 		}
 		Point2D centroid = getCentroid(points, multipliers);
 		return centroid;
+	}
+	@EventHandler
+	public void recalculateReinforcementEvent(PlayerMoveEvent e){
+		Random random = new Random();
+		if (random.nextInt(10) != 1) return;
+		Vector velocity = new Vector(e.getFrom().getX() - e.getTo().getX(),e.getFrom().getY() - e.getTo().getY(), e.getFrom().getZ() - e.getTo().getZ());
+		if (Math.abs(velocity.getX()) <= 0.15 && Math.abs(velocity.getZ()) <= 0.15) return;
+		if (e.getTo() == null) return;
+
+		new BukkitRunnable(){
+			@Override
+			public void run(){
+				recalculateReinforcement(e.getPlayer());
+
+			}
+		}.runTask(Last_life_III.getPlugin());
+	}
+
+	public static void recalculateReinforcement(Player player){
+		if (!player.getWorld().getEnvironment().equals(World.Environment.NORMAL)) return;
+		if (!playersBlocks.containsKey(player.getUniqueId()) || !uuidjsonObjectHashMap.containsKey(player.getUniqueId())) return;
+
+
+
+		JSONArray playerDataPoints = playersBlocks.get(player.getUniqueId());
+		int i = 0;
+		for (Object object : (JSONArray) playerDataPoints.clone()){
+			if (object == null) {
+				throw new RuntimeException("Object is null at i = " + i);
+			}
+			JSONObject jsonObject = (JSONObject) object;
+			if (Math.abs(Integer.parseInt(jsonObject.get("X").toString())-player.getLocation().getX()) > 10
+					&& Math.abs(Integer.parseInt(jsonObject.get("Z").toString())-player.getLocation().getZ()) > 10) {
+				if (Double.parseDouble(jsonObject.get("reinforcement").toString()) == 1) continue;
+				if (Double.parseDouble(jsonObject.get("reinforcement").toString()) >= REINFORCEMENT_CAP) {
+					Double newReinforcement = REINFORCEMENT_CAP;
+				} else {
+					Double newReinforcement = (Double.parseDouble(jsonObject.get("reinforcement").toString()) - 1) / REINFORCEMENT_DECAY + 1;
+
+				}
+
+				jsonObject.replace("reinforcement", newReinforcement);
+				playerDataPoints.remove(i);
+				playerDataPoints.add(i, jsonObject);
+			}
+			i++;
+		}
+
+		playersBlocks.replace(player.getUniqueId(), playerDataPoints);
+		uuidjsonObjectHashMap.get(player.getUniqueId()).changeData("dataPoints", playerDataPoints);
 	}
 	public static Point2D getCentroid(ArrayList<Point> points, ArrayList<Double> multiplier) {
 		double x = 0.0;
@@ -154,9 +282,9 @@ public class PlayerInformationRecopilador implements Listener {
 			return 20;
 		return 1;
 	}
-	public static Double calculateDecay(int epochSecond){
+	public static Double calculateDecay(int epochSecond, double reinforcement){
 		long currentEpochSecond = System.currentTimeMillis()/1000;
-		return (1d/(currentEpochSecond-epochSecond + 1d));
+		return 1d/((currentEpochSecond/DECAY_MULTIPLIER-epochSecond/DECAY_MULTIPLIER + 1d)/reinforcement);
 	}
 	private static Block mark = null;
 	public static void mark(World world) {
@@ -173,7 +301,6 @@ public class PlayerInformationRecopilador implements Listener {
 
 				mark = location.getBlock();
 				mark.setType(Material.REDSTONE_BLOCK);
-				Bukkit.broadcastMessage(mark.getLocation().toString());
 			}
 		}.runTaskTimer(Last_life_III.getPlugin(), 1, 5);
 	}
