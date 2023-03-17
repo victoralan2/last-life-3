78dd1848e31c163d714468a59573f392aba4c034
Mario_55 <mario.gandia.caballero@gmail.com>
2023-03-17 20:57:24 +0100

    Fixed things.

--- src/main/java/net/olimpium/last_life_iii/Enchants/precisionEnchant.java
+++ src/main/java/net/olimpium/last_life_iii/Enchants/precisionEnchant.java
@@ -1,22 +1,28 @@
 package net.olimpium.last_life_iii.Enchants;
 
 
+import io.papermc.paper.enchantments.EnchantmentRarity;
+import net.kyori.adventure.text.Component;
 import net.olimpium.last_life_iii.Last_life_III;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.enchantments.EnchantmentTarget;
+import org.bukkit.entity.EntityCategory;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityShootBowEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 
 public class precisionEnchant extends Enchantment implements Listener {
@@ -85,5 +91,35 @@ public class precisionEnchant extends Enchantment implements Listener {
     @Override
     public boolean canEnchantItem(ItemStack itemStack) {
         return true;
+    }
+
+    @Override
+    public @NotNull Component displayName(int i) {
+        return null;
+    }
+
+    @Override
+    public boolean isTradeable() {
+        return false;
+    }
+
+    @Override
+    public boolean isDiscoverable() {
+        return false;
+    }
+
+    @Override
+    public @NotNull EnchantmentRarity getRarity() {
+        return null;
+    }
+
+    @Override
+    public float getDamageIncrease(int i, @NotNull EntityCategory entityCategory) {
+        return 0;
+    }
+
+    @Override
+    public @NotNull Set<EquipmentSlot> getActiveSlots() {
+        return null;
     }
 }
